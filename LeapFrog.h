7efac65c838686b5c3519c97477586f7379fe5cf
dmorad1004 <dmorad@unal.edu.co>
2020-06-22 09:22:01 -0500

    se modificó leap-frog para el rebote con dos pelotas

--- LeapFrog.h
+++ LeapFrog.h
@@ -1,11 +1,11 @@
-#ifndef _frog_
-#define _frog_
+#ifndef _FROG_
+#define _FROG_
 
 
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include<cmath>
 struct Particle
 {
     // atributes
@@ -19,10 +19,9 @@ struct Particle
 
 struct Config {
 
-    float DT = 0.0;
+    float K = 0.0;
     float G = 0.0;
     float B = 0.0;
-    float K = 0.0;
     // method
     void read(const std::string& fname);
 };
@@ -30,7 +29,8 @@ struct Config {
 void compute_force(Particle& b, const Config& c);
 void start_integration(Particle& b, float dt);
 void timestep(float dt, Particle& b);
-
-
+float DotProduct(const float vector1[3], const float vector2[3]);
+void BallBallBounce(Particle& a, Particle& b, int& collisions_counter);
+float VectorNorm(const float vector[3]);
 
 #endif // 
